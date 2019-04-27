82d0979f1574653f823473ca2da18abc3e925bd9
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2019-04-27 19:51:13 +0530

    initial

---
+++ DependencyInjection/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private IEmployeeService employeeService { get; set; }
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(this.employeeService.GetEmployees());
+        }
+    }
+}
---
+++ DependencyInjection/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            Employee emp1 = new Employee() { EmployeeId = 1, EmployeeName = "Rasitha", Telephone = "119" };
+            Employee emp2 = new Employee() { EmployeeId = 1, EmployeeName = "Kamal", Telephone = "118" };
+            employeeList.Add(emp1);
+            employeeList.Add(emp2);
+
+            return employeeList;
+        }
+    }
+}
